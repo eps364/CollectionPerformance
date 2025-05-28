bcfa743ec6c49a667f234aa2c2b6d7a8bfd40697
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-05-27 22:56:54 -0300

    Fix formatting and variable naming in TestPerformance for consistency

--- src/performance/TestPerformance.java
+++ src/performance/TestPerformance.java
@@ -11,21 +11,21 @@ import java.util.TreeSet;
 public class TestPerformance {
 
 	public static void main(String[] args) {
-		Integer[] valores = { 30000, 50000, 100000, 150000, 200000,300000, 500000 };
+		Integer[] valores = { 30000, 50000, 100000, 150000, 200000, 300000, 500000 };
 		Collection<Integer> arrayListTest = new ArrayList<>();
 		Collection<Integer> hashSetTest = new HashSet<>();
-		Collection<Integer> hashTreeSet = new TreeSet<>();
+		Collection<Integer> treeSetTest = new TreeSet<>();
 
 		Instant initial = Instant.now();
 		Arrays.stream(valores).forEach(valor -> {
 			CollectionPerformance(arrayListTest, valor);
 			CollectionPerformance(hashSetTest, valor);
-			CollectionPerformance(hashTreeSet, valor);
+			CollectionPerformance(treeSetTest, valor);
 		});
 		Instant fim = Instant.now();
 		Duration duration = Duration.between(initial, fim);
 		System.out.println("Tempo: " + duration.toMillis() + " ms");
-		
+
 	}
 
 	private static void CollectionPerformance(Collection<Integer> teste, Integer valor) {
@@ -36,12 +36,11 @@ public class TestPerformance {
 		for (int i = 0; i < valor; i++)
 			teste.contains(i);
 
-		System.out.println(String.format("%-10s | \tTempo: %-10d milisegundos para \t%-10d elementos.", 
-			    getCollectionType(teste), (System.currentTimeMillis() - inicio), valor));
+		System.out.println(String.format("%-10s | \tTempo: %-10d milisegundos para \t%-10d elementos.",
+				getCollectionType(teste), (System.currentTimeMillis() - inicio), valor));
 	}
 
 	private static String getCollectionType(Collection<Integer> teste) {
 		return teste.getClass().getSimpleName();
 	}
 }
-
